aaaf6bdac036db9b54fa8549bccbc89f2438460a
Rizap-tomoki <tomoki.sato@rizapgroup.com>
2023-08-25 11:04:10 +0900

    #5  コントローラの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to :action => "index"
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
