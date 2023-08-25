0d64c25cbf68f975fa5a30fb73356898ac5bf029
Rizap-tomoki <tomoki.sato@rizapgroup.com>
2023-08-25 11:01:50 +0900

    #5  LESSON2(resoucesを使わない)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      redirect_to :action => "index"
     else
       render :new, status: :unprocessable_entity
     end
