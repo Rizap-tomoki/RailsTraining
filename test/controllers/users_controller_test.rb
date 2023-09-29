7c594c4f2afd2a714fc8b8032c82ab2171af7715
Rizap-tomoki <tomoki.sato@rizapgroup.com>
2023-09-29 14:31:26 +0900

    #10 コントローラの機能テスト実装

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @tanaka = users(:tanaka)
+    @user = users(:user)
   end
 
   # 各テストの実行後に呼ばれる
@@ -12,7 +12,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "indexアクションにリクエストし、レスポンスが正常にアクセスできるか確認するテスト" do
-    get user_url(@tanaka)
+    get users_url
     assert_response :success
   end
 
@@ -21,33 +21,77 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "Userのカウントが増加し、適切なリダイレクトが行われることを確認するテスト" do
+  test "User投稿確認テスト" do
     assert_difference("User.count") do
-      post users_url, params: { user: { name: "田中" } }
+      post users_url, params: {
+        user: {
+          name: "佐藤智希",
+          hiragana_nama: "サトウトモキ",
+          sex: "男性",
+          tel: "00-0000-0000",
+          mobile: "000-0000-0000",
+          mail: "example@example.com",
+          postcode: "000-0000",
+          address1: "宮城県",
+          address2: "栗原市",
+          address3: "栗駒",
+          address4: "沼倉",
+          address5: "日照田3",
+          birthday: "2001-03-14"
+        }
+      }
     end
     assert_redirected_to user_path(User.last)
   end
 
   test "showアクションにリクエストし、レスポンスが正常にアクセスできるか確認するテスト" do
-    get user_url(@tanaka)
+    get user_url(@user)
     assert_response :success
   end
 
   test "editアクションにリクエストし、レスポンスが正常にアクセスできるか確認するテスト" do
-    get edit_user_url(@tanaka)
+    get edit_user_url(@user)
     assert_response :success
   end
 
   test "リダイレクト、データベースへの変更、およびデータの一致を検証するテスト" do
-    patch user_url(@tanaka), params: { user: { name: "updated" } }
-    assert_redirected_to user_path(@tanaka)
-    @tanaka.reload
-    assert_equal "updated", @tanaka.name
+    patch user_url(@user), params: {
+      user: {
+        name: "新しい名前",
+        hiragana_nama: "アタラシイヒラガナ",
+        sex: "女性", 
+        tel: "111-1111-1111",
+        mobile: "222-2222-2222",
+        mail: "new@example.com",
+        postcode: "123-4567",
+        address1: "神奈川県",
+        address2: "新しい市",
+        address3: "新しい町",
+        address4: "新しい住所",
+        address5: "新しい番地",
+        birthday: "2000-01-01"
+      }
+    }
+    assert_redirected_to user_path(@user)
+    @user.reload
+    assert_equal "新しい名前", @user.name
+    assert_equal "アタラシイヒラガナ", @user.hiragana_nama
+    assert_equal "女性", @user.sex
+    assert_equal "111-1111-1111", @user.tel
+    assert_equal "222-2222-2222", @user.mobile
+    assert_equal "new@example.com", @user.mail
+    assert_equal "123-4567", @user.postcode
+    assert_equal "神奈川県", @user.address1
+    assert_equal "新しい市", @user.address2
+    assert_equal "新しい町", @user.address3
+    assert_equal "新しい住所", @user.address4
+    assert_equal "新しい番地", @user.address5
+    assert_equal "2000-01-01", @user.birthday.to_s
   end
 
   test "Userのカウントが減少し、rootパスにリダイレクトが行われることを確認するテスト" do
     assert_difference("User.count", -1) do
-      delete user_url(@tanaka)
+      delete user_url(@user)
     end
 
     assert_redirected_to root_path
