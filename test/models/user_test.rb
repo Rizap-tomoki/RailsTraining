8650ce1db8103b332ad11bd7a158ea8c2227adfd
tomoki.sato <tomoki.sato@rizapgroup.com>
2023-10-30 10:42:50 +0900

    #10 18歳以上だけが投稿できるように基準日を設けてモデルテストを実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,23 +1,55 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  def test_the_truth
-    assert true
-  end
-  test "名前のないuserを保存しない" do
-    user = User.new
-    assert_not user.save, "Saved the user without a name"
+
+  setup do
+    # テスト環境の日付を4月2日に設定
+    Timecop.freeze(Date.new(Date.current.year, 4, 2))
   end
   
-  test "エラーの表示" do
-    # some_undefined_variableはテストケースのどこにも定義されていない
-    assert_raises(NameError) do
-      some_undefined_variable
-    end
+  teardown do
+    # Timecopで設定した日付をリセット
+    Timecop.return
   end
 
   test "全項目の入力のない投稿を保存しない" do
     user = User.new
     assert_not user.save, "Saved the user without a name"
+  end
+
+  test "基準日によって18歳以上になっていないことの検出" do
+    under_18_person = User.new(
+      name: "佐藤智希",
+      hiragana_nama: "サトウトモキ",
+      sex: "男性",
+      tel: "00-000-0000",
+      mobile: "000-0000-0000",
+      mail: "example@example.com",
+      postcode: "000-0000",
+      address1: "宮城県",
+      address2: "栗原市",
+      address3: "栗駒",
+      address4: "沼倉",
+      birthday: Date.new(2005, 4, 3)
+    )
+    assert_not under_18_person.valid?  # 18歳未満の人はバリデーションエラーが発生
+  end
+
+  test "基準日によって18歳以上になっていることを検出" do
+    over_18_person = User.new(
+      name: "佐藤智希",
+      hiragana_nama: "サトウトモキ",
+      sex: "男性",
+      tel: "111-000-0000",
+      mobile: "111-0000-0000",
+      mail: "111example@example.com",
+      postcode: "000-0000",
+      address1: "宮城県",
+      address2: "栗原市",
+      address3: "栗駒",
+      address4: "沼倉",
+      birthday: Date.new(2005, 4, 1)
+    )  
+    assert over_18_person.valid?     # 18歳以上の人はバリデーションエラーが発生しない
   end
 end
