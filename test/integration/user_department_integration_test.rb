6688613fec042d4d8f8c9d6a840b36b9599b2c70
tomoki.sato <tomoki.sato@rizapgroup.com>
2023-11-13 15:47:33 +0900

    #12 結合テストを削除

--- test/integration/user_department_integration_test.rb
+++ test/integration/user_department_integration_test.rb
@@ -2,17 +2,5 @@ require "test_helper"
 
 class UserDepartmentIntegrationTest < ActionDispatch::IntegrationTest
 
-  setup do
-    @user = users(:user)
-    @department = departments(:department)
-  end
-  
-  teardown do
-    Rails.cache.clear
-  end
-
-  test "ユーザーと部署の結合テスト" do
-    assert_includes @department.users, @user
-  end
 
 end
