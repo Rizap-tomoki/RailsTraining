1af22ad29aaa4162525434bc5c18e7800270586a
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-02-29 13:16:26 +0900

    #32 一般公開ページで次の検索、絞り込み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.page(params[:page])
-    end
+  def index
+    @users = User.all
+    @users = @users.where("name LIKE ?", "%#{params[:user_name_search]}%") if params[:user_name_search].present?
+    @users = @users.where(address1: params[:user_address1_search]) if params[:user_address1_search].present?
+    @users = @users.order(birthday: :desc) if params[:birthday_sort] == "latest"
+    @users = @users.order(birthday: :asc) if params[:birthday_sort] == "oldest"
+    @users = @users.page(params[:page]).per(params[:per_page])
+  end
   
-    def show
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 end
   
