847f1516842900df0c52cec4ba4f354eb9a5aad2
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-03-01 11:27:02 +0900

    #32 コントローラ・ビュー（address1）の編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = User.page(params[:page]).per(params[:per_page])
     @users = @users.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
     @users = @users.where(address1: params[:address1]) if params[:address1].present?
-    @users = @users.order(birthday: params[:birthday].to_sym) if params[:birthday].present?
+    @users = @users.order(birthday: params[:birthday]) if params[:birthday].present?
   end
   
   def show
