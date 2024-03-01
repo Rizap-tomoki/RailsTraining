bedb6cff0dd3f7eb483582d7dea1e2047e5811ad
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-03-01 09:27:07 +0900

    #32 controllerとviewの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    @users = @users.where("name LIKE ?", "%#{params[:user_name_search]}%") if params[:user_name_search].present?
-    @users = @users.where(address1: params[:user_address1_search]) if params[:user_address1_search].present?
-    @users = @users.order(birthday: :desc) if params[:birthday_sort] == "latest"
-    @users = @users.order(birthday: :asc) if params[:birthday_sort] == "oldest"
-    @users = @users.page(params[:page]).per(params[:per_page])
+    @users = User.page(params[:page]).per(params[:per_page])
+    @users = @users.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    @users = @users.where(address1: params[:address1]) if params[:address1].present?
+    @users = @users.order(birthday: params[:birthday].to_sym) if params[:birthday].present?
   end
   
   def show
