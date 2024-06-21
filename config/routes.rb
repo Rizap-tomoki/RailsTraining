28e7f12b0794019e73ff62e747cedc847ff298f2
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-06-21 17:57:17 +0900

    routes.rbの削除

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,4 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   root "users#index"
   # Defines the root path route ("/")
-  match "*path", to: "application#record_or_routing_not_found", via: :all
 end
