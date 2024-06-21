61887cf3db421a046b6ece604642d5f708be4e04
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-06-21 11:54:14 +0900

    #53 404ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   root "users#index"
   # Defines the root path route ("/")
+  match "*path", to: "application#render_404", via: :all
 end
