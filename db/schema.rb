df37cf10c4acdd29bd1e95b0f4c40392562e1e26
tomoki.sato <tomoki.sato@rizapgroup.com>
2024-06-24 14:58:35 +0900

    #50 indexで余計のところを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_20_071036) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_24_030124) do
   create_table "departments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_departments_on_name"
   end
 
   create_table "skills", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_skills_on_name"
   end
 
   create_table "skills_users", id: false, charset: "utf8mb4", force: :cascade do |t|
